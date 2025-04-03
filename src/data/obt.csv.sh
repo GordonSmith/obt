b23b494524ff18944f2391cb9318ed551525cc9a
Gordon Smith <GordonJSmith@gmail.com>
2025-04-03 13:53:36 +0100

    chore: Switch to new gist
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- src/data/obt.csv.sh
+++ src/data/obt.csv.sh
@@ -1,3 +1,3 @@
-git clone https://gist.github.com/38579c76f1e3efc7f2bb91907ed8bc5d.git src/.observablehq/cache/obt-data || true
+git clone https://gist.github.com/908d9834c9400b24037670d4b23364cc.git src/.observablehq/cache/obt-data || true
 
 find src/.observablehq/cache/obt-data -type f -exec stat --format="%n" {} \;
